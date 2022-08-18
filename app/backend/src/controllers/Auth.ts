6394418744980530f3caf58bf73c36eccec371b0
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-08-18 20:55:43 -0300

    fix: erro ao redirecionar

--- app/backend/src/controllers/Auth.ts
+++ app/backend/src/controllers/Auth.ts
@@ -5,7 +5,6 @@ import service from '../services/Auth';
 export default class Auth {
   static async authenticate(req: Request, res: Response) {
     const token: IToken | undefined = await service.authenticate(req.body);
-    res.status(200).json(token);
-    res.redirect('/matches');
+    res.status(200).json(token);//.redirect('/matches');
   }
 }
