4b46088e73d0bf8523aa4b74dfa3f0ad800ff0da
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-08-18 19:36:42 -0300

    feat: middleware erro criado o middleware de erro

---
+++ app/backend/src/middlewares/error.ts
@@ -0,0 +1,27 @@
+import { ErrorRequestHandler } from "express";
+
+const errorMiddleware: ErrorRequestHandler = (err, _req, res, _next) => {
+  const { name, message } = err;
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({ message });
+      break;
+    case 'Unauthorized':
+      res.status(401).json({ message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'ConflictError':
+      res.status(409).json({ message });
+      break;
+    case 'SequelizeConnectionRefusedError':
+      res.status(503).end();
+      break;
+    default:
+      res.status(500).json({ message });
+      break;
+  }
+}
+
+export default errorMiddleware;
